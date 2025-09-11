50d7bb76203a9fe4f76762d2141e4c630d74b5f7
Qi Zhang <113491364+winnerzq@users.noreply.github.com>
2025-09-11 15:21:37 +0800

    main.py

--- main.py
+++ main.py
@@ -13,32 +13,25 @@ from utils import create_train_test_split, create_pyg_data, plot_results
 from config import LOCATIONS, WINDOW_SIZE, HMM_STATES, SAVE_PATH
 
 if __name__ == "__main__":
-    # 数据加载
     data_dict, site_names = load_data('chaohu-processed-data.xlsx')
 
-    # 构建空间图
     edge_index, edge_weight = build_spatial_graph(LOCATIONS, site_names)
 
-    # 特征工程
     hmm_feat_array, _ = extract_hmm_features(data_dict, n_states=HMM_STATES, window_size=WINDOW_SIZE)
     X, y = build_dataset(data_dict, hmm_feat_array, window_size=WINDOW_SIZE)
 
-    # 数据集划分
     train_mask = create_train_test_split(site_names)
 
-    # 构建PyG Data
     data = create_pyg_data(X, y, edge_index, edge_weight, train_mask)
 
-    # 初始化模型
     model = EnhancedGCN(in_dim=data.x.shape[1])
 
-    # 训练与评估
     trained_model, losses = train_and_evaluate(model, data, site_names, save_path=SAVE_PATH)
 
-    # 可视化结果
     trained_model.eval()
     with torch.no_grad():
         pred = trained_model(data).cpu().numpy()
         y_true = data.y.cpu().numpy()
+
 
     plot_results(y_true, pred, data.train_mask, losses)
